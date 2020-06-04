7eb4b90932944cef6a8f38644eabb81754fce67f
nicoXXXXXtitan <nicoc2010@gmail.com>
2020-06-04 08:49:17 +0200

    modify composant NavCategory

--- React/src/components/HomeUser/PageHome/NavCategory/index.js
+++ React/src/components/HomeUser/PageHome/NavCategory/index.js
@@ -1,13 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import 'src/components/HomeUser/PageHome/NavCategory/navCategory.scss';
-
 import { Nav } from 'react-bootstrap';
 
 const NavCategory = ({ listTypes, getCategory }) => {
-
-  const arrayTypes = [];
-  listTypes.map((item) => (arrayTypes.push(item.type)));
 
   const onClick = (evt) => {
     evt.preventDefault();
@@ -16,15 +12,15 @@ const NavCategory = ({ listTypes, getCategory }) => {
 
   return (
     <Nav className="navCategory mt-2 ">
-      {listTypes && arrayTypes.map((type) => (
+      {listTypes && listTypes.map((item) => (
         <Nav.Item>
           <Nav.Link
             className="navItems"
-            key={type}
-            name={type}
+            key={item.type}
+            name={item.type}
             onClick={onClick}
           >
-            {type}
+            {item.type}
           </Nav.Link>
         </Nav.Item>
       ))}
